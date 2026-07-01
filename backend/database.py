283c95e53ba69872fcfad5316523fbf6bcf59785
ahmadxxxk22-code <ahmadxxxk22@gmail.com>
2026-07-01 06:01:06 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 from config import settings
 
 engine = create_engine(
@@ -12,6 +13,9 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
