b015b3ef3909831bb881ddd5460bd54d815d25c9
ahmadxxxk22-code <ahmadxxxk22@gmail.com>
2026-07-03 04:40:55 +0300

    Update binance_service.py

--- backend/app/services/binance_service.py
+++ backend/app/services/binance_service.py
@@ -1,16 +1,16 @@
 import requests
 
+from app.services.base_market_service import BaseMarketService
 
-class BinanceService:
+
+class BinanceService(BaseMarketService):
 
     BASE_URL = "https://api.binance.com/api/v3"
 
     def get_price(self, symbol="BTCUSDT"):
 
-        url = f"{self.BASE_URL}/ticker/price"
-
         response = requests.get(
-            url,
+            f"{self.BASE_URL}/ticker/price",
             params={"symbol": symbol},
             timeout=10
         )
@@ -26,10 +26,8 @@ class BinanceService:
 
     def get_24h(self, symbol="BTCUSDT"):
 
-        url = f"{self.BASE_URL}/ticker/24hr"
-
         response = requests.get(
-            url,
+            f"{self.BASE_URL}/ticker/24hr",
             params={"symbol": symbol},
             timeout=10
         )
@@ -53,3 +51,48 @@ class BinanceService:
             "volume": float(data["volume"])
 
         }
+
+    def get_candles(
+        self,
+        symbol="BTCUSDT",
+        interval="1h",
+        limit=200
+    ):
+
+        response = requests.get(
+            f"{self.BASE_URL}/klines",
+            params={
+                "symbol": symbol,
+                "interval": interval,
+                "limit": limit
+            },
+            timeout=10
+        )
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        candles = []
+
+        for candle in data:
+
+            candles.append({
+
+                "open_time": candle[0],
+
+                "open": float(candle[1]),
+
+                "high": float(candle[2]),
+
+                "low": float(candle[3]),
+
+                "close": float(candle[4]),
+
+                "volume": float(candle[5]),
+
+                "close_time": candle[6]
+
+            })
+
+        return candles
