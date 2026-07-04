a979a4dc98cfe656a45b20eba1fcdf12b8aaf574
ahmadxxxk22-code <ahmadxxxk22@gmail.com>
2026-07-05 01:20:59 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,51 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
 from config import settings
 
+DATABASE_URL = settings.DATABASE_URL
+
+engine_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    engine_args = {
+        "connect_args": {
+            "check_same_thread": False
+        }
+    }
+
 engine = create_engine(
-    settings.DATABASE_URL,
-    pool_pre_ping=True
+    DATABASE_URL,
+    pool_pre_ping=True,
+    future=True,
+    **engine_args
 )
 
 SessionLocal = sessionmaker(
+    bind=engine,
     autocommit=False,
-    autoflush=False,
-    bind=engine
+    autoflush=False
 )
 
 Base = declarative_base()
 
 
 def get_db():
+
     db = SessionLocal()
+
     try:
+
         yield db
+
     finally:
+
         db.close()
+
+
+def create_database():
+
+    Base.metadata.create_all(
+        bind=engine
+    )
