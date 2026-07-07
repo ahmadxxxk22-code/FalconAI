1222135e30427b03515673be33b982beb67cf0e4
ahmadxxxk22-code <ahmadxxxk22@gmail.com>
2026-07-07 04:35:28 +0300

    Update email_service.py

--- backend/app/auth/email_service.py
+++ backend/app/auth/email_service.py
@@ -1,1 +1,61 @@
+import smtplib
 
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+
+EMAIL_ADDRESS = ""
+EMAIL_PASSWORD = ""
+
+
+class EmailService:
+
+    @staticmethod
+    def send_email(
+        to_email: str,
+        subject: str,
+        body: str
+    ):
+
+        if not EMAIL_ADDRESS or not EMAIL_PASSWORD:
+            return False
+
+        message = MIMEMultipart()
+
+        message["From"] = EMAIL_ADDRESS
+        message["To"] = to_email
+        message["Subject"] = subject
+
+        message.attach(
+            MIMEText(
+                body,
+                "plain"
+            )
+        )
+
+        try:
+
+            server = smtplib.SMTP(
+                SMTP_SERVER,
+                SMTP_PORT
+            )
+
+            server.starttls()
+
+            server.login(
+                EMAIL_ADDRESS,
+                EMAIL_PASSWORD
+            )
+
+            server.send_message(message)
+
+            server.quit()
+
+            return True
+
+        except Exception:
+
+            return False
