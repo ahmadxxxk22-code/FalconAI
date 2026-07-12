56d2879dc40bfaf5c0a4b28b078004d88a375def
ahmadxxxk22-code <ahmadxxxk22@gmail.com>
2026-07-12 08:17:22 +0300

    Update base_market_service.py

--- backend/app/services/base_market_service.py
+++ backend/app/services/base_market_service.py
@@ -3,19 +3,44 @@ from abc import ABC, abstractmethod
 
 class BaseMarketService(ABC):
 
+
+    @property
     @abstractmethod
-    def get_price(self, symbol: str):
+    def provider_name(self):
+
         pass
 
+
+
     @abstractmethod
-    def get_24h(self, symbol: str):
+    def get_price(
+        self,
+        symbol: str,
+        market: str = "crypto"
+    ):
+
         pass
+
+
+
+    @abstractmethod
+    def get_24h(
+        self,
+        symbol: str,
+        market: str = "crypto"
+    ):
+
+        pass
+
+
 
     @abstractmethod
     def get_candles(
         self,
         symbol: str,
         interval: str,
-        limit: int = 100
+        limit: int = 100,
+        market: str = "crypto"
     ):
+
         pass
